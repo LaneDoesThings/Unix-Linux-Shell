262e29666336c554986a16464362d202f238febf
LaneDoesThings <ltw0014@uah.edu>
2024-10-05 16:10:37 -0500

    echo and ps1 command

--- src/mysh.c
+++ src/mysh.c
@@ -4,35 +4,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *strremove(char *str, const char *sub);
+
 int main(int argc, char *argv[])
 {
     FILE *fileA, *fileB;
     char *command, *args[10], termbuf[256];
     bool exit = false;
+    char *prompt = "$";
 
     while (!exit)
     {
-        printf("$ ");
+        printf("%s ", prompt);
         fgets(termbuf, sizeof(termbuf), stdin);
-        for (size_t i = 0; termbuf[i]; ++i)
-        {
-            if (termbuf[i] == '\n')
-                termbuf[i] = '\0';
-        }
+
+        // Remove trailing newline
+        termbuf[strcspn(termbuf, "\n")] = '\0';
 
         command = strtok(termbuf, " ");
         if (command == NULL)
             continue;
-    
-        if (strcmp(command, "cp") == 0)
+
+        if (strcmp(command, "echo") == 0)
         {
-            args[0] = strtok(0, " ");
+            args[0] = strtok(NULL, "\0");
+            char *options = strchr(args[0], '-');
+            if (options != NULL)
+            {
+                args[1] = strtok(strdup(options), " ");
+                args[2] = strdup(args[0]);
+                char* test = strcat(strdup(args[1]), " ");
+                args[2] = strremove(args[2], test);
+                args[2] = strremove(args[2], args[1]);
+                args[2] = strtok(args[2], "\"");
+                printf("%s", args[2]);
+            }
+            else
+            {
+                args[2] = strtok(strdup(args[0]), "\"");
+                printf("%s\r\n", args[2]);
+            }
+        }
+
+        else if (strcmp(command, "PS1") == 0)
+        {
+            args[0] = strtok(NULL, "\0");
+            prompt = strdup(args[0]);
+            continue;
+        }
+
+        else if (strcmp(command, "cp") == 0)
+        {
+            args[0] = strtok(NULL, " ");
             if (args[0] == NULL)
             {
                 fputs("Input file is missing\n", stderr);
                 continue;
             }
-            args[1] = strtok(0, " ");
+            args[1] = strtok(NULL, " ");
             if (args[1] == NULL)
             {
                 fputs("Output file is missing\n", stderr);
@@ -64,4 +93,17 @@ int main(int argc, char *argv[])
     }
 
     return EXIT_SUCCESS;
+}
+
+
+//From https://stackoverflow.com/questions/47116974/remove-a-substring-from-a-string-in-c
+char *strremove(char *str, const char *sub) {
+    size_t len = strlen(sub);
+    if (len > 0) {
+        char *p = str;
+        while ((p = strstr(p, sub)) != NULL) {
+            memmove(p, p + len, strlen(p + len) + 1);
+        }
+    }
+    return str;
 }
